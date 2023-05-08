0bfad6e57c00e56bf8632ade377597fab7ac1d50
AshrafHabromman <ashraf.habrumman@gmail.com>
2023-05-08 23:29:31 +0300

    Implement Red unit test of empty string

---
+++ String Calculator Kata/String Calculator Kata. Tests/StringCalculatorTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace String_Calculator_Kata._Tests
+{
+    public class StringCalculatorTest
+    {
+        [Fact]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            var stringCalculator = new StringCalculator();
+
+            int actual = stringCalculator.Add("");
+
+            Assert.Equal(0, actual);
+
+        }
+    }
+}
---
+++ String Calculator Kata/String Calculator Kata/StringCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace String_Calculator_Kata._Tests
+{
+    public class StringCalculator
+    {
+        public StringCalculator()
+        {
+        }
+
+        public int Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
