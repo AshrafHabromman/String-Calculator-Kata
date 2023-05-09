9c6ccdacbf6ca9ef68fc9fabf593a34bc7d6c898
AshrafHabromman <ashraf.habrumman@gmail.com>
2023-05-09 11:45:15 +0300

    Implement the red part of test for one number

--- String Calculator Kata/String Calculator Kata. Tests/StringCalculatorTest.cs
+++ String Calculator Kata/String Calculator Kata. Tests/StringCalculatorTest.cs
@@ -13,5 +13,17 @@ namespace String_Calculator_Kata._Tests
             Assert.Equal(0, actual);
 
         }
+
+        [Fact]
+        public void Add_OneNumber_ReturnsItself()
+        {
+            var stringCalculator = new StringCalculator();
+
+            int actual = stringCalculator.Add("1");
+            int expected = 1;
+
+            Assert.Equal(expected, actual);
+
+        }
     }
 }
